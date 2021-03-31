fd4eaf0d7847d5371ce9cdb1264ee8e5b1fbcd86
yesraaj11 <78349719+yesraaj11@users.noreply.github.com>
2021-03-31 17:41:04 +0530

    Create setup.py

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ setuptools.setup(
         "Framework :: AsyncIO",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     entry_points="""
         [console_scripts]
