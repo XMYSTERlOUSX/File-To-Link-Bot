c852b36ec73f11edae029873df8dd67dad81e812
yesraaj11 <78349719+yesraaj11@users.noreply.github.com>
2021-03-31 11:09:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setuptools.setup(
     extras_require={
         "fast": ["cryptg>=0.2"],
     },
-    python_requires="~=3.8",
+    python_requires="~=3.7",
 
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -37,8 +37,8 @@ setuptools.setup(
         "Framework :: AsyncIO",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
     ],
     entry_points="""
         [console_scripts]
